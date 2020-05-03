6c467a5040589ce87d97966911ed25b90a7468eb
Armando <picondev@gmail.com>
2020-05-03 18:39:53 -0400

    initial commit

---
+++ coroutineexample2/src/main/java/pe/devpicon/android/coroutineexample2/MainActivity.kt
@@ -0,0 +1,24 @@
+package pe.devpicon.android.coroutineexample2
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_main.*
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        btn_launch.setOnClickListener {
+            CoroutineScope(Dispatchers.IO).launch{
+
+
+
+            }
+        }
+    }
+}
