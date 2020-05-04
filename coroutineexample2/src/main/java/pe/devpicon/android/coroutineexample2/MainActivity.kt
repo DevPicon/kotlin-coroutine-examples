45432e3247d7c523ea0d6efa98e6d7afac5f3ab8
Armando <picondev@gmail.com>
2020-05-04 10:31:11 -0400

    Second example

--- coroutineexample2/src/main/java/pe/devpicon/android/coroutineexample2/MainActivity.kt
+++ coroutineexample2/src/main/java/pe/devpicon/android/coroutineexample2/MainActivity.kt
@@ -1,11 +1,13 @@
 package pe.devpicon.android.coroutineexample2
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
 import kotlinx.android.synthetic.main.activity_main.*
 import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.delay
 import kotlinx.coroutines.launch
+import kotlin.concurrent.thread
 
 class MainActivity : AppCompatActivity() {
 
@@ -14,7 +16,35 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
 
         btn_launch.setOnClickListener {
-
+            val coroutineScope = CoroutineScope(Dispatchers.Main)
+            coroutineScope.launch {
+                log("start launch")
+                val product = getProductById("123")
+                log(product.toString())
+                log("end onClickListener()")
+            }
         }
+    }
+
+    private suspend fun getProductById(productId: String): Product {
+        delay(3000)
+        return Product(productId, "Fideos")
+    }
+
+    private suspend fun getProductByIdStandard(productId: String): Product {
+        Thread.sleep(3000)
+        return Product(productId, "Fideos")
+    }
+
+    private fun getProductById(productId: String,
+                               callback: (Product) -> Unit) {
+        thread {
+            Thread.sleep(3000)
+            callback(Product(productId, "Fideos"))
+        }
+    }
+
+    private fun log(message: String) {
+        println("${Thread.currentThread().name} example 02: $message")
     }
 }
---
+++ coroutineexample2/src/main/java/pe/devpicon/android/coroutineexample2/Product.kt
@@ -0,0 +1,3 @@
+package pe.devpicon.android.coroutineexample2
+
+data class Product(val id: String, val name: String)
